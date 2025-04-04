f2591055f9d82ec8b3bc92a6d6e9ebd5ca336aca
Christian <christian-matthias.goellner@stud.uni-bamberg.de>
2025-04-04 13:55:33 +0200

    Further results

--- linear_regression/Statement.sql
+++ linear_regression/Statement.sql
@@ -3,6 +3,6 @@ WITH RECURSIVE gd (idx, a, b) AS (
 UNION ALL
     SELECT idx+1, a-rate*avg(2*x*(a*x+b-y)), b-rate*avg(2*(a*x+b-y))
     FROM gd, points, lr
-    WHERE idx < 5 GROUP BY idx, a, b
+    WHERE idx < 5 GROUP BY idx, a, b, rate
 )
 SELECT * FROM gd WHERE idx = 5;
