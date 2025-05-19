614ef419ea33e881e4769af835716a2748e812d5
Christian20003 <christian.goellner@bnv-bamberg.de>
2025-05-19 14:31:46 +0200

    Reworked iris

--- iris_regression/Config.py
+++ iris_regression/Config.py
@@ -10,6 +10,120 @@ UMBRA_DB_DATABASE_FILE = 'iris'
 POSTGRES_DB_DATABASE_FILE = 'iris'
 STATEMENT_FILE = 'Statement.sql'
 
+STATEMENT_1 = '''
+with recursive w (iter,id,i,j,v) as (
+  (select 0,0,* from w_xh union select 0,1,* from w_ho)
+  union all
+  (
+  with w_now as (
+     SELECT * from w
+  ), a_xh(i,j,v) as (
+     SELECT m.i, n.j, 1/(1+exp(-{} (m.v*n.v)))
+     FROM img AS m INNER JOIN w_now AS n ON m.j=n.i
+     WHERE n.id=0 and n.iter=(select max(iter) from w_now) -- w_xh
+     GROUP BY m.i, n.j
+  ), a_ho(i,j,v) as (
+     SELECT m.i, n.j, 1/(1+exp(-{} (m.v*n.v))) --sig(SUM (m.v*n.v))
+     FROM a_xh AS m INNER JOIN w_now AS n ON m.j=n.i
+     WHERE n.id=1 and n.iter=(select max(iter) from w_now)  -- w_ho
+     GROUP BY m.i, n.j
+  ), l_ho(i,j,v) as (
+     select m.i, m.j, 2*(m.v-n.v)
+     from a_ho AS m INNER JOIN one_hot AS n ON m.i=n.i AND m.j=n.j
+  ), d_ho(i,j,v) as (
+     select m.i, m.j, m.v*n.v*(1-n.v)
+     from l_ho AS m INNER JOIN a_ho AS n ON m.i=n.i AND m.j=n.j
+  ), l_xh(i,j,v) as (
+     SELECT m.i, n.i as j, ({} (m.v*n.v)) -- transpose
+     FROM d_ho AS m INNER JOIN w_now AS n ON m.j=n.j
+     WHERE n.id=1 and n.iter=(select max(iter) from w_now)  -- w_ho
+     GROUP BY m.i, n.i
+  ), d_xh(i,j,v) as (
+     select m.i, m.j, m.v*n.v*(1-n.v)
+     from l_xh AS m INNER JOIN a_xh AS n ON m.i=n.i AND m.j=n.j
+  ), d_w(id,i,j,v) as (
+     SELECT 0, m.j as i, n.j, ({} (m.v*n.v))
+     FROM img AS m INNER JOIN d_xh AS n ON m.i=n.i
+     GROUP BY m.j, n.j
+     union
+     SELECT 1, m.j as i, n.j, ({} (m.v*n.v))
+     FROM a_xh AS m INNER JOIN d_ho AS n ON m.i=n.i
+     GROUP BY m.j, n.j
+  )
+  select iter+1, w.id, w.i, w.j, w.v - 0.01 * d_w.v
+  from w_now as w, d_w
+  where iter < {} and w.id=d_w.id and w.i=d_w.i and w.j=d_w.j
+  )
+)
+
+SELECT iter, count(*)::float/(select count(distinct i) from one_hot) AS precision
+FROM (
+   SELECT *, rank() over (partition by m.i,iter order by v desc) as rank
+   FROM (
+      SELECT m.i, n.j, 1/(1+exp(-{} (m.v*n.v))) as v, m.iter
+      FROM (
+         SELECT m.i, n.j, 1/(1+exp(-{} (m.v*n.v))) as v, iter
+         FROM img AS m INNER JOIN w AS n ON m.j=n.i
+         WHERE n.id=0 -- and n.iter=(select max(iter) from w)
+         GROUP BY m.i, n.j, iter ) AS m INNER JOIN w AS n ON m.j=n.i
+      WHERE n.id=1 and n.iter=m.iter
+      GROUP BY m.i, n.j, m.iter
+   ) m ) pred,
+   (SELECT *, rank() over (partition by m.i order by v desc) as rank FROM one_hot m) test
+WHERE pred.i=test.i and pred.rank = 1 and test.rank=1
+GROUP BY iter, pred.j=test.j
+HAVING (pred.j=test.j)=true
+ORDER BY iter;\n
+'''
+
+STATEMENT_2 = '''
+with recursive w (iter,id,i,j,v) as (
+  (select 0,0,* from w_xh union select 0,1,* from w_ho)
+  union all
+  (
+  with w_now as (
+     SELECT * from w
+  ), a_xh(i,j,v) as (
+     SELECT m.i, n.j, 1/(1+exp(-{} (m.v*n.v)))
+     FROM img AS m INNER JOIN w_now AS n ON m.j=n.i
+     WHERE n.id=0 and n.iter=(select max(iter) from w_now) -- w_xh
+     GROUP BY m.i, n.j
+  ), a_ho(i,j,v) as (
+     SELECT m.i, n.j, 1/(1+exp(-{} (m.v*n.v))) --sig(SUM (m.v*n.v))
+     FROM a_xh AS m INNER JOIN w_now AS n ON m.j=n.i
+     WHERE n.id=1 and n.iter=(select max(iter) from w_now)  -- w_ho
+     GROUP BY m.i, n.j
+  ), l_ho(i,j,v) as (
+     select m.i, m.j, 2*(m.v-n.v)
+     from a_ho AS m INNER JOIN one_hot AS n ON m.i=n.i AND m.j=n.j
+  ), d_ho(i,j,v) as (
+     select m.i, m.j, m.v*n.v*(1-n.v)
+     from l_ho AS m INNER JOIN a_ho AS n ON m.i=n.i AND m.j=n.j
+  ), l_xh(i,j,v) as (
+     SELECT m.i, n.i as j, ({} (m.v*n.v)) -- transpose
+     FROM d_ho AS m INNER JOIN w_now AS n ON m.j=n.j
+     WHERE n.id=1 and n.iter=(select max(iter) from w_now)  -- w_ho
+     GROUP BY m.i, n.i
+  ), d_xh(i,j,v) as (
+     select m.i, m.j, m.v*n.v*(1-n.v)
+     from l_xh AS m INNER JOIN a_xh AS n ON m.i=n.i AND m.j=n.j
+  ), d_w(id,i,j,v) as (
+     SELECT 0, m.j as i, n.j, ({} (m.v*n.v))
+     FROM img AS m INNER JOIN d_xh AS n ON m.i=n.i
+     GROUP BY m.j, n.j
+     union
+     SELECT 1, m.j as i, n.j, ({} (m.v*n.v))
+     FROM a_xh AS m INNER JOIN d_ho AS n ON m.i=n.i
+     GROUP BY m.j, n.j
+  )
+  select iter+1, w.id, w.i, w.j, w.v - 0.01 * d_w.v
+  from w_now as w, d_w
+  where iter < {} and w.id=d_w.id and w.i=d_w.i and w.j=d_w.j
+  )
+)
+SELECT * FROM w;\n
+'''
+
 CONFIG = {
     'learning_rate': 0.01,
     'databases': [
@@ -21,7 +135,9 @@ CONFIG = {
             'csv_header': [
                 'Type', 
                 'Network_Size', 
-                'Data_Size', 
+                'Data_Size',
+                'Aggregation',
+                'Learning_Rate', 
                 'Iterations', 
                 'Execution', 
                 'Heap', 
@@ -34,7 +150,8 @@ CONFIG = {
             'execution-bench': f'{Settings.DUCK_DB_PATH} -json -f {STATEMENT_FILE} {DUCK_DB_DATABASE_FILE}',
             'start-sql': [],
             'end-sql': ['.exit'],
-            'types': ['float', 'bfloat']
+            'types': ['double', 'float', 'bfloat'],
+            'aggregations': ['standard', 'kahan']
         },
         {
             'name': 'umbra',
@@ -44,29 +161,22 @@ CONFIG = {
             'csv_header': [
                 'Type', 
                 'Network_Size', 
-                'Data_Size', 
-                'Iterations',  
+                'Data_Size',
+                'Aggregation',
+                'Learning_Rate', 
+                'Iterations', 
                 'Execution', 
                 'Heap', 
                 'RSS', 
-                'Loss', 
                 'Umbra', 
                 'Tensorflow'
                 ],
-            'files': [
-                f'./{UMBRA_DB_DATABASE_FILE}', 
-                f'./{UMBRA_DB_DATABASE_FILE}.0_0.data', 
-                f'./{UMBRA_DB_DATABASE_FILE}.0_0.sample', 
-                f'./{UMBRA_DB_DATABASE_FILE}.0_0s.data', 
-                f'./{UMBRA_DB_DATABASE_FILE}.lock', 
-                f'./{UMBRA_DB_DATABASE_FILE}.pages', 
-                f'./{UMBRA_DB_DATABASE_FILE}.wal'
-                ],
+            'files': [Settings.UMBRA_DIR],
             'execution': f'{Settings.UMBRA_DB_PATH} -createdb {UMBRA_DB_DATABASE_FILE}',
             'execution-bench': f'{Settings.UMBRA_DB_PATH} {UMBRA_DB_DATABASE_FILE} {STATEMENT_FILE}',
             'start-sql': [],
-            'end-sql': ['\q;'],
-            'types': ['float']
+            'types': ['float8', 'float'],
+            'aggregations': ['standard']
         },
         {
             'name': 'postgres',
@@ -76,12 +186,13 @@ CONFIG = {
             'csv_header': [
                 'Type', 
                 'Network_Size', 
-                'Data_Size', 
+                'Data_Size',
+                'Aggregation',
+                'Learning_Rate', 
                 'Iterations', 
                 'Execution', 
                 'Heap', 
                 'RSS', 
-                'Loss', 
                 'Postgres', 
                 'Tensorflow'
                 ],
@@ -90,12 +201,14 @@ CONFIG = {
                 f'{Settings.POSTGRESQL_DB_PATH}initdb -D {Settings.POSTGRESQL_DIR} -U {Settings.POSTGRESQL_USERNAME}', 
                 f'{Settings.POSTGRESQL_DB_PATH}pg_ctl -D {Settings.POSTGRESQL_DIR} start', 
                 f'{Settings.POSTGRESQL_DB_PATH}createdb -h {Settings.POSTGRESQL_HOST} -p {Settings.POSTGRESQL_PORT} {POSTGRES_DB_DATABASE_FILE} -U {Settings.POSTGRESQL_USERNAME}', 
-                f'{Settings.POSTGRESQL_DB_PATH}pg_ctl -D {Settings.POSTGRESQL_DIR} stop'],
+                f'{Settings.POSTGRESQL_DB_PATH}pg_ctl -D {Settings.POSTGRESQL_DIR} stop',
+                f'{Settings.POSTGRESQL_DB_PATH}postgres -D {Settings.POSTGRESQL_DIR} -p {Settings.POSTGRESQL_PORT} -h {Settings.POSTGRESQL_HOST}'],
             'execution': f'{Settings.POSTGRESQL_DB_PATH}psql -h {Settings.POSTGRESQL_HOST} -p {Settings.POSTGRESQL_PORT} -U {Settings.POSTGRESQL_USERNAME} -d {POSTGRES_DB_DATABASE_FILE}',
             'execution-bench': f'{Settings.POSTGRESQL_DB_PATH}psql -h {Settings.POSTGRESQL_HOST} -p {Settings.POSTGRESQL_PORT} -U {Settings.POSTGRESQL_USERNAME} -d {POSTGRES_DB_DATABASE_FILE} -f {STATEMENT_FILE}',
             'start-sql': [],
             'end-sql': [],
-            'types': ['float']
+            'types': ['float8', 'float4'],
+            'aggregations': ['standard']
         }
     ],
     'setups': [
@@ -103,139 +216,170 @@ CONFIG = {
             'iterations': 100,
             'network_size': 100,
             'data_size': 150,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 100,
             'data_size': 300,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 100,
             'data_size': 600,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 100,
             'data_size': 1200,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 100,
             'data_size': 2400,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 100,
             'data_size': 4800,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 300,
             'data_size': 150,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 300,
             'data_size': 300,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 300,
             'data_size': 600,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 300,
             'data_size': 1200,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 300,
             'data_size': 2400,
+            'statements': [STATEMENT_1],
             'ignore': False
         },
         {
             'iterations': 100,
             'network_size': 300,
             'data_size': 4800,
+            'statements': [STATEMENT_1],
+            'ignore': False
+        },
+        {
+            'iterations': 100,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_1],
+            'ignore': False
+        },
+        # Setups with increased iterations
+        {
+            'iterations': 1,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 2,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 3,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 4,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 5,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 6,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 7,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 8,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 9,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 10,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_2],
+            'ignore': False
+        },
+        {
+            'iterations': 10,
+            'network_size': 300,
+            'data_size': 4800,
+            'statements': [STATEMENT_1],
             'ignore': False
         }
     ] 
 }
-STATEMENT = '''
-with recursive w (iter,id,i,j,v) as (
-  (select 0,0,* from w_xh union select 0,1,* from w_ho)
-  union all
-  (
-  with w_now as (
-     SELECT * from w
-  ), a_xh(i,j,v) as (
-     SELECT m.i, n.j, 1/(1+exp(-SUM (m.v*n.v)))
-     FROM img AS m INNER JOIN w_now AS n ON m.j=n.i
-     WHERE n.id=0 and n.iter=(select max(iter) from w_now) -- w_xh
-     GROUP BY m.i, n.j
-  ), a_ho(i,j,v) as (
-     SELECT m.i, n.j, 1/(1+exp(-SUM (m.v*n.v))) --sig(SUM (m.v*n.v))
-     FROM a_xh AS m INNER JOIN w_now AS n ON m.j=n.i
-     WHERE n.id=1 and n.iter=(select max(iter) from w_now)  -- w_ho
-     GROUP BY m.i, n.j
-  ), l_ho(i,j,v) as (
-     select m.i, m.j, 2*(m.v-n.v)
-     from a_ho AS m INNER JOIN one_hot AS n ON m.i=n.i AND m.j=n.j
-  ), d_ho(i,j,v) as (
-     select m.i, m.j, m.v*n.v*(1-n.v)
-     from l_ho AS m INNER JOIN a_ho AS n ON m.i=n.i AND m.j=n.j
-  ), l_xh(i,j,v) as (
-     SELECT m.i, n.i as j, (SUM (m.v*n.v)) -- transpose
-     FROM d_ho AS m INNER JOIN w_now AS n ON m.j=n.j
-     WHERE n.id=1 and n.iter=(select max(iter) from w_now)  -- w_ho
-     GROUP BY m.i, n.i
-  ), d_xh(i,j,v) as (
-     select m.i, m.j, m.v*n.v*(1-n.v)
-     from l_xh AS m INNER JOIN a_xh AS n ON m.i=n.i AND m.j=n.j
-  ), d_w(id,i,j,v) as (
-     SELECT 0, m.j as i, n.j, (SUM (m.v*n.v))
-     FROM img AS m INNER JOIN d_xh AS n ON m.i=n.i
-     GROUP BY m.j, n.j
-     union
-     SELECT 1, m.j as i, n.j, (SUM (m.v*n.v))
-     FROM a_xh AS m INNER JOIN d_ho AS n ON m.i=n.i
-     GROUP BY m.j, n.j
-  )
-  select iter+1, w.id, w.i, w.j, w.v - 0.01 * d_w.v
-  from w_now as w, d_w
-  where iter < {} and w.id=d_w.id and w.i=d_w.i and w.j=d_w.j
-  )
-)
-
-SELECT MAX(precision) FROM (
-    SELECT iter, count(*)::float/(select count(distinct i) from one_hot) AS precision
-    FROM (
-       SELECT *, rank() over (partition by m.i,iter order by v desc) as rank
-       FROM (
-          SELECT m.i, n.j, 1/(1+exp(-SUM (m.v*n.v))) as v, m.iter
-          FROM (
-             SELECT m.i, n.j, 1/(1+exp(-SUM (m.v*n.v))) as v, iter
-             FROM img AS m INNER JOIN w AS n ON m.j=n.i
-             WHERE n.id=0 -- and n.iter=(select max(iter) from w)
-             GROUP BY m.i, n.j, iter ) AS m INNER JOIN w AS n ON m.j=n.i
-          WHERE n.id=1 and n.iter=m.iter
-          GROUP BY m.i, n.j, m.iter
-       ) m ) pred,
-       (SELECT *, rank() over (partition by m.i order by v desc) as rank FROM one_hot m) test
-    WHERE pred.i=test.i and pred.rank = 1 and test.rank=1
-    GROUP BY iter, pred.j=test.j
-    HAVING (pred.j=test.j)=true
-    ORDER BY iter);\n
-'''
--- iris_regression/iris.py
+++ iris_regression/iris.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../shar
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../shared/Helper')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../shared/Global')))
 
-from Config import CONFIG, STATEMENT, STATEMENT_FILE
+from Config import CONFIG, STATEMENT_1, STATEMENT_2, STATEMENT_FILE
 import Format
 import Database
 import Create_CSV
@@ -29,54 +29,115 @@ def main():
     for scenario in scenarios:
         if scenario['ignore']:
             continue
-        generate_statement(scenario['iterations'])
         for database in databases:
             if database['ignore']:
                 continue
             for type in database['types']:
-                Format.print_title(f'START BENCHMARK - IRIS-REGRESSION WITH HIDDEN_LAYER_WIDTH: {scenario["network_size"]} AND {scenario["data_size"]} SAMPLES, TYPE: {type}, DATABASE: {database["name"]}')
-                if database['name'] == 'postgres':
-                    executables = database['prep']
-                    Postgres.create_database(executables[0], executables[1], executables[2])
-                prep_database = Database.Database(database['execution'], database['start-sql'], database['end-sql'])
-                prep_database.create_table('iris', ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'], [type, type, type, type, 'int'])
-                prep_database.create_table('iris2', ['id', 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'], ['int', type, type, type, type, 'int'])
-                prep_database.create_table('img', ['i', 'j', 'v'], ['int', 'int', type])
-                prep_database.create_table('one_hot', ['i', 'j', 'v', 'dummy'], ['int', 'int', 'int', 'int'])
-                prep_database.create_table('w_xh', ['i', 'j', 'v'], ['int', 'int', type])
-                prep_database.create_table('w_ho', ['i', 'j', 'v'], ['int', 'int', type])
-                for _ in range(0, scenario['data_size'], 150):
-                    prep_database.insert_from_csv('iris', './iris.csv')
-                prep_database.insert_from_select('iris2', 'SELECT row_number() OVER (), * FROM iris')
-                prep_database.insert_from_select('img', 'SELECT id, 1, sepal_length/10 FROM iris2')
-                prep_database.insert_from_select('img', 'SELECT id, 2, sepal_width/10 FROM iris2')
-                prep_database.insert_from_select('img', 'SELECT id, 3, petal_length/10 FROM iris2')
-                prep_database.insert_from_select('img', 'SELECT id, 4, petal_width/10 FROM iris2')
-                prep_database.insert_from_select('one_hot', 'select n.i, n.j, coalesce(i.v,0), i.v from (select id,species+1 as species,1 as v from iris2) i right outer join (select a.a as i, b.b as j from (select generate_series as a from generate_series(1,150)) a, (select generate_series as b from generate_series(1,4)) b) n on n.i=i.id and n.j=i.species order by i,j')
-                prep_database.insert_from_select('w_xh', f'select i.*,j.*,random()*2-1 from generate_series(1,4) i, generate_series(1,{scenario["network_size"]}) j')
-                prep_database.insert_from_select('w_ho', f'select i.*,j.*,random()*2-1 from generate_series(1,{scenario["network_size"]}) i, generate_series(1,3) j')
-                prep_database.execute_sql()
+                for statement in scenario['statements']:
+                    statement_number = 0
+                    if (statement == STATEMENT_1):
+                        statement_number = 1
+                    elif (statement == STATEMENT_2):
+                        statement_number = 2
+                    for agg in database['aggrations']:
+                        Format.print_title(f'START BENCHMARK - IRIS-REGRESSION WITH HIDDEN_LAYER_WIDTH: {scenario["network_size"]} AND {scenario["data_size"]} SAMPLES, TYPE: {type}, DATABASE: {database["name"]}')
+                        generate_statement(statement, scenario['iterations'], agg)
+                        prepare_benchmark(database, type, scenario['data_size'], scenario['network_size'])
 
-                time, output = Time.benchmark(database['execution-bench'], database['name'], 1, [0])
-                heap, rss = Memory.benchmark(database['execution-bench'], f'{database["name"]}_{type}_{scenario["data_size"]}_{scenario["network_size"]}')
+                        time, output = Time.benchmark(database['execution-bench'], database['name'], 2, [0, 1])
+                        server = []
+                        if database['name'] == 'postgres':
+                            Postgres.stop_database(database['prep'][3])
+                            server = [database['prep'][4], database['prep'][3]]
+                        heap, rss = Memory.benchmark(database['name'], database['execution-bench'], f'{database["name"]}_{type}_{scenario["data_size"]}_{scenario["network_size"]}_{agg}_{statement_number}', server)
 
-                Create_CSV.append_row(database['csv_file'], [type, scenario["network_size"], scenario["data_size"], scenario['iterations'], time, heap, rss, output, '-'])
-                Helper.remove_files(database['files'])
-                if database['name'] == 'postgres':
-                    Postgres.stop_database(database['prep'][3])
-                    Helper.remove_dir(Settings.POSTGRESQL_DIR)
+                        Create_CSV.append_row(database['csv_file'], [
+                            type, 
+                            scenario["network_size"], 
+                            scenario["data_size"], 
+                            agg,
+                            CONFIG['learning_rate'],
+                            scenario['iterations'], 
+                            time, 
+                            heap, 
+                            rss, 
+                            output, 
+                            '-'])
+                        if database['name'] == 'postgres' or database['name'] == 'umbra':
+                            Helper.remove_dir(database['files'])
+                        else:
+                            Helper.remove_files(database['files'])
     Helper.remove_files([STATEMENT_FILE])
 
+def prepare_benchmark(database: dict, type: str, data_size: int, network_size: int) -> None:
+    '''
+    This function prepares the benchmark by creating the database and inserting the data.
 
-def generate_statement(iterations: int) -> None:
+    :param database: The database name.
+    :param type: The datatype for x and y values.
+    :param data_size: The number of samples.
+    :param network_size: The number of neurons in the hidden layer.
+    '''
+
+    Format.print_information('Preparing benchmark - This can take some time', mark=True)
+    extend_file_path = '.' if database['name'] == 'postgres' else ''
+    if database['name'] == 'postgres':
+        os.mkdir(Settings.POSTGRESQL_DIR)
+        executables = database['prep']
+        Postgres.create_database(executables[0], executables[1], executables[2])
+    elif database['name'] == 'umbra':
+        os.mkdir(Settings.UMBRA_DIR)
+
+    prep_database = Database.Database(database['execution'], database['start-sql'], database['end-sql'])
+    prep_database.create_table('iris', ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'], [type, type, type, type, 'int'])
+    prep_database.create_table('iris2', ['id', 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'], ['int', type, type, type, type, 'int'])
+    prep_database.create_table('img', ['i', 'j', 'v'], ['int', 'int', type])
+    prep_database.create_table('one_hot', ['i', 'j', 'v', 'dummy'], ['int', 'int', 'int', 'int'])
+    prep_database.create_table('w_xh', ['i', 'j', 'v'], ['int', 'int', type])
+    prep_database.create_table('w_ho', ['i', 'j', 'v'], ['int', 'int', type])
+    for _ in range(0, data_size, 150):
+        prep_database.insert_from_csv('iris', extend_file_path + './iris.csv')
+    prep_database.insert_from_select('iris2', 'SELECT row_number() OVER (), * FROM iris')
+    prep_database.insert_from_select('img', 'SELECT id, 1, sepal_length/10 FROM iris2')
+    prep_database.insert_from_select('img', 'SELECT id, 2, sepal_width/10 FROM iris2')
+    prep_database.insert_from_select('img', 'SELECT id, 3, petal_length/10 FROM iris2')
+    prep_database.insert_from_select('img', 'SELECT id, 4, petal_width/10 FROM iris2')
+    prep_database.insert_from_select('one_hot', 'select n.i, n.j, coalesce(i.v,0), i.v from (select id,species+1 as species,1 as v from iris2) i right outer join (select a.a as i, b.b as j from (select generate_series as a from generate_series(1,150)) a, (select generate_series as b from generate_series(1,4)) b) n on n.i=i.id and n.j=i.species order by i,j')
+    prep_database.insert_from_select('w_xh', f'select i.*,j.*,random()*2-1 from generate_series(1,4) i, generate_series(1,{network_size}) j')
+    prep_database.insert_from_select('w_ho', f'select i.*,j.*,random()*2-1 from generate_series(1,{network_size}) i, generate_series(1,3) j')
+    prep_database.execute_sql()
+
+def generate_statement(statement: str, iterations: int, aggr_func: str) -> None:
     '''
     This function generates the SQL file.
 
+    :param statement: The SQL statement to be executed.
     :param iterations: The number of iterations in the recursive CTE.
+    :param aggr_func: The aggregation function to be used.
     '''
 
+    agg_func = 'SUM' if agg_func == 'standard' else 'KAHAN_SUM'
     with open(STATEMENT_FILE, 'w') as file:
-        file.write(STATEMENT.format(iterations))
+        if statement == STATEMENT_1:
+            file.write(statement.format(
+                agg_func,
+                agg_func,
+                agg_func,
+                agg_func,
+                agg_func,
+                iterations,
+                agg_func,
+                agg_func
+            ))
+        else:
+            file.write(statement.format(
+                agg_func,
+                agg_func,
+                agg_func,
+                agg_func,
+                agg_func,
+                iterations
+            ))
 
 if __name__ == '__main__':
     main()
